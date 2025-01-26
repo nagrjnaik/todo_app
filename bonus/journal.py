503f6729ca353b476074f8ea8b6040a46937bddc
naikn <naiknagrj@gmail.com>
2025-01-26 18:26:55 +0530

    minor code changes to the journal creation program.

--- bonus/journal.py
+++ bonus/journal.py
@@ -1,6 +1,7 @@
 date = input("Enter today's date (DD-MM-YYYY format): ")
 mood = input("Enter today's mood: ")
-journal = input("Enter your thoughts for today.\n").capitalize()
+thoughts = input("Enter your thoughts for today.\n").capitalize()
 
 with open(date + '.txt', 'w') as file:
-    file.write(journal)
+    file.write(f"Today's mood is {mood}/10!\n")
+    file.write(thoughts)
