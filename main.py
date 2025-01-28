26b415792abe9f24d2ecb4efb0c0b505a50c5acb
naikn <naiknagrj@gmail.com>
2025-01-28 20:46:31 +0530

    # fixed a bug in edit section. refactored the code to add function to write file methods. # removed redundant files # added bonus12 example

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-def get_todos():
-    with open('Todo_list.txt', 'r') as file_local:
+def get_todos(filepath):
+    with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()  # local variable name should be different from global variables
         return todos_local
+
+
+def write_todos(filepath, todos_arg):
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
 
 while True:
     user_action = input("choose add, show, edit, remove or exit: ")
@@ -10,15 +16,16 @@ while True:
         todo = user_action[4:] + '\n'  # string slicing to extract from user input after add
         todo = todo.capitalize()
 
-        todos = get_todos()
+        todos = get_todos(filepath='Todo_list.txt')  # alternate way to write the code for better readability. not always needed.
 
         todos.append(todo)
-        with open('Todo_list.txt', 'w') as file:
-            file.writelines(todos)
+
+        write_todos(todos_arg=todos,filepath='Todo_list.txt')
 
     elif user_action.startswith("show"):
 
-        todos = get_todos()
+        todos = get_todos('Todo_list.txt')
+        
         # list comprehension -- same as below code
         # new_todos = [item.strip('\n') for item in todos]
         for index, item in enumerate(todos):
@@ -27,27 +34,32 @@ while True:
 
     elif user_action.startswith("edit"):
         try:
-            todos = get_todos()
+            todos = get_todos('Todo_list.txt')
             number = int(user_action[5:])
-            number = number - 1 #adjust for indexing
-            new_todo = input("Enter a new todo: ") + '\n'
+            number = number - 1  # adjust for indexing
+            # 1 2 3 4 --> user input
+            # 0 1 2 3 --> index adjustment
+            if number < 0 or number >= len(todos):  # ensure index is valid  # this removes redundant input choice when the index in wrong
+                print("There is no item with that number.")
+                continue
+            else:
+                new_todo = input("Enter a new todo: ") + '\n'
             todos[number] = new_todo.capitalize()
-            with open('Todo_list.txt', 'w') as file:
-                file.writelines(todos)
+
+            write_todos('Todo_list.txt',todos)
+
         except ValueError:
             print("Your command is not valid.")
             continue  # opposite of break! restarts the code cycle from top
-        except IndexError:
-            print("There is no item with that number.")
-            continue
 
     elif user_action.startswith("remove"):
         try:
-            todos = get_todos()
+            todos = get_todos('Todo_list.txt')
             number = int(user_action[7:])
             todos.pop(number - 1)
-            with open('Todo_list.txt', 'w') as file:
-                file.writelines(todos)
+
+            write_todos(todos_arg=todos, filepath='Todo_list.txt')
+
         except IndexError:
             print("There is no item with that number.")
             continue
