10cb8d6889b187bb89d15d6937e9b797bf09f50c
naikn <naiknagrj@gmail.com>
2025-04-08 19:13:34 +0530

    Implement "Add new todo" feature

--- webapp.py
+++ webapp.py
@@ -3,8 +3,15 @@ import functions #backend to pass the todos
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("To-Do List") #title
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a new todo...")
+st.text_input(label="", placeholder="Add a new todo...",
+              on_change=add_todo, key="new_todo")
