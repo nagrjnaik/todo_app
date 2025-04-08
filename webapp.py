7ee44288a089d31ed40bc933ca802460f9e8e2b4
naikn <naiknagrj@gmail.com>
2025-04-08 19:34:53 +0530

    Implement "clear todo on checkbox tick" feature

--- webapp.py
+++ webapp.py
@@ -1,17 +1,26 @@
 import streamlit as st
+from streamlit import session_state
+
 import functions #backend to pass the todos
 
 todos = functions.get_todos()
 
 def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
+    todo1 = st.session_state["new_todo"] + "\n"
+    todos.append(todo1)
     functions.write_todos(todos)
+    st.session_state["new_todo"] = ""  # Clear the input field
 
 
 st.title("To-Do List") #title
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    #key holds the session state
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Add a new todo...",
               on_change=add_todo, key="new_todo")
